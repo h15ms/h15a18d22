0b1cfe197f417bc40760344bff3f4c58e046bb2d
brij.singh <brij.singh@harkemedia.com>
2017-01-13 17:53:12 +0530

    sql file
    sql file

--- db/db.sql
+++ db/db.sql
@@ -1,1 +1,392 @@
 
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 13, 2017 at 05:48 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `mi__counsulting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrator`
+--
+
+CREATE TABLE IF NOT EXISTS `administrator` (
+  `id` int(22) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `dob` datetime NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `alt_mobile` varchar(15) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `status` enum('0','1') NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `administrator`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `area`
+--
+
+CREATE TABLE IF NOT EXISTS `area` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `locality_id` bigint(22) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `area`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE IF NOT EXISTS `city` (
+  `id` int(22) NOT NULL AUTO_INCREMENT,
+  `name` int(100) NOT NULL,
+  `iso2` varchar(10) NOT NULL,
+  `iso3` varchar(10) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `status` enum('1','0') NOT NULL COMMENT '1 - active , 0 - block',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `city`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consult_qa`
+--
+
+CREATE TABLE IF NOT EXISTS `consult_qa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `consult_qa`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `iso2` varchar(10) NOT NULL,
+  `iso3` varchar(10) NOT NULL,
+  `status` enum('0','1') NOT NULL COMMENT '1- Active   0 - block',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `country`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE IF NOT EXISTS `doctor` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `dob` datetime NOT NULL,
+  `fees` int(10) NOT NULL,
+  `time_table` int(200) NOT NULL,
+  `education` int(200) NOT NULL,
+  `specialization` varchar(255) NOT NULL,
+  `experience` tinyint(4) NOT NULL,
+  `service` varchar(200) NOT NULL,
+  `registration_number` varchar(25) NOT NULL,
+  `registration_organisation` varchar(100) NOT NULL,
+  `awards` varchar(200) NOT NULL,
+  `avatar` varchar(100) NOT NULL,
+  `others` varchar(200) NOT NULL,
+  `status` enum('1','0') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `doctor`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` int(22) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `dob` datetime NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `alt_mobile` varchar(15) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `status` enum('0','1') NOT NULL COMMENT '1 - Active , 0 - Block',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  `customer_id` varchar(22) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `feedback`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees_collection`
+--
+
+CREATE TABLE IF NOT EXISTS `fees_collection` (
+  `id` bigint(22) NOT NULL,
+  `amt` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fees_collection`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE IF NOT EXISTS `gallery` (
+  `token` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `alt` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `status` enum('0','1') NOT NULL COMMENT '1- Active   0 - block',
+  PRIMARY KEY (`token`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gallery`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital`
+--
+
+CREATE TABLE IF NOT EXISTS `hospital` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `hospital`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locality`
+--
+
+CREATE TABLE IF NOT EXISTS `locality` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `status` enum('1','0') NOT NULL,
+  PRIMARY KEY (`id`,`status`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `locality`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE IF NOT EXISTS `patient` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `dob` datetime NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `alt_mobile` varchar(15) NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `passport_number` varchar(50) NOT NULL,
+  `status` enum('0','1') NOT NULL COMMENT '1 - Active , 0 - Block',
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `patient`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(25) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `status` enum('PENDING','DONE','INPROCESS') NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `payment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `point` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `reviews`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE IF NOT EXISTS `state` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `iso3` varchar(10) NOT NULL,
+  `iso2` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `state`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tour_package`
+--
+
+CREATE TABLE IF NOT EXISTS `tour_package` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `package_name` varchar(100) NOT NULL,
+  `amt` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `tour_package`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transport`
+--
+
+CREATE TABLE IF NOT EXISTS `transport` (
+  `id` int(11) NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `name` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transport`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visa`
+--
+
+CREATE TABLE IF NOT EXISTS `visa` (
+  `id` bigint(22) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `visa`
+--
