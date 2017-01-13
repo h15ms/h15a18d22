a9daceda6c59ce0cca9a5483d385972d6de9af89
Ankit Vikalp <ankit.vikalp@harkemedia.com>
2017-01-13 09:46:00 +0530

    commit

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,11 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-<<<<<<< HEAD:application/controllers/home.php
-class home extends CI_Controller {
-=======
+
 class home  extends CI_Controller {
->>>>>>> origin/master:application/controllers/home.php
+
 
 	
 	public function index()
