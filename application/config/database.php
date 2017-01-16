f73d3a38f804f7e74b121854c50e7ca5bff682f7
harpreet <harpreet.kaur@harkemedia.com>
2017-01-16 11:22:19 +0530

    commit

--- application/config/database.php
+++ application/config/database.php
@@ -76,7 +76,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.5.20',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'miconsulting',
