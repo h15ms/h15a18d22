daef73dacd6fdc44b0f9ec210b9d5bbc8a2a87d3
Ankit Vikalp <ankit.vikalp@harkemedia.com>
2017-01-31 10:45:17 +0530

    commit

--- application/controllers/admin/Homecrm.php
+++ application/controllers/admin/Homecrm.php
@@ -5,17 +5,19 @@ class Homecrm extends CI_Controller {
 
   public function __construct() {
       
-     parent:: __construct();
-    
-    error_reporting(0);
+      parent:: __construct();
+      
+      error_reporting(0);
 
-    if(isset($_SESSION['logged_in'])){
-      $sess = $this->session->userdata();
-      if(( $sess['logged_in']['user_level'] != '1' && $sess['logged_in']['user_level_status'] != '1' ) || ( $sess['logged_in']['user_level'] != '2' && $sess['logged_in']['user_level_status'] != '1' ) ){  redirect('home','refresh');}//header ('Location: '.base_url().'home '); }
-    }else{
-      redirect('login','refresh');
-    }      
+      if(isset($_SESSION['logged_in'])){
+        $sess = $this->session->userdata();
+        if(( $sess['logged_in']['user_level'] != '1' && $sess['logged_in']['user_level_status'] != '1' ) || ( $sess['logged_in']['user_level'] != '2' && $sess['logged_in']['user_level_status'] != '1' ) ){  redirect('home','refresh');}//header ('Location: '.base_url().'home '); }
+      }else{
+        redirect('login','refresh');
+      }      
 
+      $this->load->model('admin/HomeModelCrm' , 'hmc'); 
+      
 
   }
 
