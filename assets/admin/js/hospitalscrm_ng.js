e28c48c9bb09dc3faeb7d95c9b29d7c181cd3584
harpreet <harpreet.kaur@harkemedia.com>
2017-02-09 17:37:21 +0530

    commit

--- assets/admin/js/hospitalscrm_ng.js
+++ assets/admin/js/hospitalscrm_ng.js
@@ -12,7 +12,6 @@ var app = angular.module('hospitalApp', []).run(function ($rootScope) {
     $http.get(BASEURL+"admin/hospitalscrm/getindexjson")
     .then(function (response) {$scope.hosPital = response.data;
     $scope.delpopupn = function(id,name){
-        alert(name);
      $('#appid').val(id);
      $('#hospitalname').html(name);
      $('#new_bankpop').modal('show');
